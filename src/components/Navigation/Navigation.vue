3a89ce5e08059fe839a1870f6ee0de690c053c8c
Franjo Eric <franjo.eric@4players.de>
2021-10-16 17:29:39 +0200

    navigation bar added

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,26 +1,44 @@
 <template>
-   <div class="d-flex justify-space-around w-100">
-       <router-link to="/">Home</router-link>
-       <router-link to="/videos">Videos</router-link>
-       <router-link to="/gallery">Gallery</router-link>
-       <router-link to="/contact">Contact</router-link>
-   </div>
+   <!--<div class="d-flex justify-space-around w-100">-->
+       <!--<router-link to="/">Home</router-link>-->
+       <!--<router-link to="/videos">Videos</router-link>-->
+       <!--<router-link to="/gallery">Gallery</router-link>-->
+       <!--<router-link to="/contact">Contact</router-link>-->
+   <!--</div>-->
+    <div class="center examplex">
+        <vs-navbar hide-scroll center-collapsed v-model="active" shadow text-white dark>
+            <template #left>
+                <img src="../../assets/logo.png" alt="" width="75px">
+            </template>
+            <vs-navbar-item to="/" :active="active === 'home'" id="home">
+                Home
+            </vs-navbar-item>
+            <vs-navbar-item to="/videos" :active="active === 'videos'" id="videos">
+                Videos
+            </vs-navbar-item>
+            <vs-navbar-item to="/gallery" :active="active === 'gallery'" id="gallery">
+                Gallery
+            </vs-navbar-item>
+            <vs-navbar-item to="/contact" :active="active === 'contact'" id="contact">
+                Contact
+            </vs-navbar-item>
+            <!--<template #right>-->
+                <!--<vs-button flat >Login</vs-button>-->
+                <!--<vs-button>Get Started</vs-button>-->
+            <!--</template>-->
+        </vs-navbar>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Navigation"
+        name: "Navigation",
+        data(){
+            return {
+                active: "home"
+            }
+        }
     }
 </script>
 
-<style scoped>
-    .d-flex{
-        display:flex;
-    }
-    .justify-space-around{
-        justify-content: space-around;
-    }
-    .w-100{
-        width:100%
-    }
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
 import Vuex from 'vuex'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
 
 Vue.use(Vuex);
 Vue.use(VueRouter);
+Vue.use(Vuesax);
 
 Vue.config.productionTip = false
 
