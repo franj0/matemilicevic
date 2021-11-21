c16a90ebdb0b0b16a649da01744a25ff4dea5986
Franjo Eric <franjo.eric@4players.de>
2021-11-21 09:05:57 +0100

    create slider

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <navigation />
-    <router-view/>
+    <div style="margin-top:75px;">
+        <router-view/>
+    </div>
     <Footer />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -5,14 +5,23 @@ import router from './router'
 import Vuex from 'vuex'
 import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css' //Vuesax styles
+import vuetify from './plugins/vuetify';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(Vuex);
 Vue.use(VueRouter);
 Vue.use(Vuesax);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
